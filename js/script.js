0fc7ea24b931bd86dc0544fe6f850d7b98a411f3
tim.stannard <tim.stannard@yoobeecolleges.com>
2022-05-30 12:07:03 +1200

    added date picker and calculating dates difference and validation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-//jquery is going to run this whole function
-// once the page has loaded
+// JQuery general function that runs when the page has loaded
 $(function() {
-  // looks for the input with the name birthday and attaches the date range picker plugin
+// grab an HTML input name of birthday
+// plug daterangepicker
   $('input[name="birthday"]').daterangepicker({
     singleDatePicker: true,
     showDropdowns: true,
@@ -11,11 +11,71 @@ $(function() {
 })
 ;
 
+const message = document.getElementById('message');
+let valid;
+
 $(function() {
   $('input[name="daterange"]').daterangepicker({
     showDropdowns: true,
+    // blank by default
+    autoUpdateInput: false,
     minYear: 1901,
     maxYear: parseInt(moment().format('YYYY'),10)
   })
-})
-;
+  $('input[name="daterange"]').on('apply.daterangepicker',
+  function(ev, picker) {
+    // console.log("You clicked apply");
+      let fullDate = $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+
+      let start = picker.startDate.format('MM/DD/YYYY');
+      let end = picker.endDate.format('MM/DD/YYYY');
+      let daysSelected = datediff(parseDate(start), parseDate(end));
+      // console.log(daysSelected);
+
+      // work out the differece between two dates
+      function parseDate(str) {
+          var mdy = str.split('/');
+          return new Date(mdy[2], mdy[0]-1, mdy[1]);
+      }
+
+      // this function here works out the difference between the Dates
+      // using two arguments, the start date and the end date
+      function datediff(first, second) {
+          // Take the difference between the dates and divide by milliseconds per day.
+          // Round to nearest whole number to deal with DST.
+          return Math.round((second-first)/(1000*60*60*24));
+      }
+      let messageContent;
+      // add in our own validation
+      if (daysSelected < 3) {
+        messageContent = `
+        Please select at least 3 days.
+        `
+        valid = false;
+      } else if (daysSelected > 16) {
+        messageContent = `
+        <span class="warning">Please select less than 16 days.</span>
+        `
+        valid = false;
+      } else {
+        messageContent = `
+        <strong>Days selected: ${daysSelected} <i class="bi bi-check-square"></i></strong>
+        `
+        valid = true;
+      }
+      message.innerHTML = messageContent;
+    }
+  )
+});
+
+const submit = document.getElementById('submit-button');
+submit.onclick = function() {
+  if (valid == true) {
+    alert("success");
+  }
+}
+
+// Activity:
+// ---------
+// Get it so if you click submit and the days are valid (use a boolean)
+// Make an alert saying "submitted successfully!"
